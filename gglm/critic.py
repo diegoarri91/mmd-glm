8a34253c32f0d04f3c6c122ed7865c89307d864e
diego <diegoarri91@gmail.com>
2020-04-13 19:07:09 -0300

    adding many in the end useless features to critic of wglm

--- gglm/critic.py
+++ gglm/critic.py
@@ -12,20 +12,30 @@ def sigmoid(x):
 
 class Critic:
     
-    def __init__(self, u_kernel=None):
+    def __init__(self, u_kernel=None, u_spk_kernel=None, r_kernel=None, r_spk_kernel=None, beta=None, features=None):
         self.u_kernel = u_kernel
-        # self.beta = beta
+        self.u_spk_kernel = u_spk_kernel
+        self.r_kernel = r_kernel
+        self.r_spk_kernel = r_spk_kernel
+        self.features = features if features is not None else []
+        self.beta = beta
         # self.x0 = x0
         # self.log_likelihood_iterations = []
         
     def copy(self):
-        return self.__class__(u_kernel=self.u_kernel.copy())
+        u_kernel = None if self.u_kernel is None else self.u_kernel.copy()
+        u_spk_kernel = None if self.u_spk_kernel is None else self.u_spk_kernel.copy()
+        r_kernel = None if self.r_kernel is None else self.r_kernel.copy()
+        r_spk_kernel = None if self.r_spk_kernel is None else self.r_spk_kernel.copy()
+        beta = None if self.beta is None else self.beta.copy()
+        return self.__class__(u_kernel=u_kernel, u_spk_kernel=u_spk_kernel, r_kernel=r_kernel, r_spk_kernel=r_spk_kernel, features=self.features.copy(), 
+                              beta=beta)
         
-    def fit(self, t, mask_spikes, u, y, newton_kwargs=None, verbose=False):
+    def fit(self, t, mask_spikes, u, r, y, newton_kwargs=None, verbose=False):
 
         newton_kwargs = {} if newton_kwargs is None else newton_kwargs
         
-        objective_kwargs = self.objective_kwargs(t, mask_spikes, u, y)
+        objective_kwargs = self.objective_kwargs(t, mask_spikes, u, r, y)
         gh_objective = partial(self.gh_objective, **objective_kwargs)
 
         optimizer = NewtonMethod(model=self, gh_objective=gh_objective, verbose=verbose, **newton_kwargs)
@@ -46,65 +56,182 @@ class Critic:
 #         n_eta = self.u_kernel.nbasis
         # theta = np.zeros((1 + len(self.beta) + n_eta))
         # theta[0] = self.x0
-        theta= self.u_kernel.coefs.copy()
+        
+        theta = []
+        if self.u_kernel is not None:
+            theta.append(self.u_kernel.coefs.copy())
+            
+        if self.u_spk_kernel is not None:
+            theta.append(self.u_spk_kernel.coefs.copy())
+            
+        if self.r_kernel is not None:
+            theta.append(self.r_kernel.coefs.copy())
+            
+        if self.r_spk_kernel is not None:
+            theta.append(self.r_spk_kernel.coefs.copy())
+            
+        ii = 0
+        if 'r_spk' in self.features:
+            theta.append(np.array([self.beta[ii]]))
+        theta = np.concatenate(theta)
         return theta
     
     def set_params(self, theta):
         # self.x0 = theta[0]
         # self.beta = theta[1:]
-#         self.u0 = theta[2]
-        self.u_kernel.coefs = theta[:]
+#         self.u0 = theta[2
+        ii = 0
+        if self.u_kernel is not None:
+            self.u_kernel.coefs = theta[ii:ii + len(self.u_kernel.coefs)]
+            ii += len(self.u_kernel.coefs)
+        
+        if self.u_spk_kernel is not None:
+            self.u_spk_kernel.coefs = theta[ii:ii + len(self.u_spk_kernel.coefs)]
+            ii += len(self.u_spk_kernel.coefs)
+        
+        if self.r_kernel is not None:
+            self.r_kernel.coefs = theta[ii:ii + len(self.r_kernel.coefs)]
+            ii += len(self.r_kernel.coefs)
+            
+        if self.r_spk_kernel is not None:
+            self.r_spk_kernel.coefs = theta[ii:ii + len(self.r_spk_kernel.coefs)]
+            ii += len(self.r_spk_kernel.coefs)
+            
+        if 'r_spk' in self.features:
+            self.beta[ii] = theta[ii]
+            ii += 1 
+        
         return self
 
-    def transform(self, t, mask_spikes, u):
+    def transform(self, t, mask_spikes, u, r):
 
         theta = self.get_params()
-        wasserstein_kwargs = self.wasserstein_distance_kwargs(t, mask_spikes, u, None)
+        wasserstein_kwargs = self.wasserstein_distance_kwargs(t, mask_spikes, u, r, None)
         X_u = wasserstein_kwargs['X_u']
+        X_u_spk = wasserstein_kwargs['X_u_spk']
+        r_spk = wasserstein_kwargs['r_spk']
+        X_r = wasserstein_kwargs['X_r']
+        X_r_spk = wasserstein_kwargs['X_r_spk']
+        
+        a = np.zeros(mask_spikes.shape[1])
 
-        X_u_theta = np.einsum('tka,a->k', X_u, theta)
-
-        a = X_u_theta
+        ii = 0
+        if X_u is not None:
+            X_u_theta = np.einsum('tka,a->k', X_u, theta[ii:ii + len(self.u_kernel.coefs)])
+            a = a + X_u_theta
+            ii += len(self.u_kernel.coefs)
+        
+        if X_u_spk is not None:
+            X_u_spk_theta = np.einsum('tka,a->k', X_u_spk, theta[ii:ii + len(self.u_spk_kernel.coefs)])
+            a = a + X_u_spk_theta
+            ii += len(self.u_spk_kernel.coefs)
+            
+        if X_r is not None:
+            X_r_theta = np.einsum('tka,a->k', X_r, theta[ii:ii + len(self.r_kernel.coefs)])
+            a = a + X_r_theta
+            ii += len(self.r_kernel.coefs)
+        
+        if X_r_spk is not None:
+            X_r_spk_theta = np.einsum('tka,a->k', X_r_spk, theta[ii:ii + len(self.r_spk_kernel.coefs)])
+            a = a + X_r_spk_theta
+            ii += len(self.r_spk_kernel.coefs)
+            
+        if r_spk is not None:
+            a = a + theta[ii] * r_spk
+            ii += 1
 
         return a
     
-    def gh_wasserstein_distance(self, dt, mask_spikes, X_u, y):
+    def gh_wasserstein_distance(self, dt, mask_spikes, X_u, X_u_spk, r_spk, X_r, X_r_spk, y):
 
         theta = self.get_params()
-
-        X_u_theta = np.einsum('tka,a->k', X_u, theta)
-        a = X_u_theta
-
+            
+        a = np.zeros(len(y))
+        g_w_distance = []
+        
+        ii = 0
+#         print('X_u', X_u)
+        if X_u is not None:
+            X_u_theta = np.einsum('tka,a->k', X_u, theta[ii:ii + len(self.u_kernel.coefs)])
+            a = a + X_u_theta
+            g_w_distance.append(np.mean(np.sum(X_u[:, y == 1, :], 0), 0) - np.mean(np.sum(X_u[:, y == 0, :], 0), 0))
+            ii += len(self.u_kernel.coefs)
+        
+        if X_u_spk is not None:
+            X_u_spk_theta = np.einsum('tka,a->k', X_u_spk, theta[ii:ii + len(self.u_spk_kernel.coefs)])
+            a = a + X_u_spk_theta
+#             print(np.mean(a[y==1]))
+            g_w_distance.append(np.mean(np.sum(X_u_spk[:, y == 1, :], 0), 0) - np.mean(np.sum(X_u_spk[:, y == 0, :], 0), 0))
+            ii += len(self.u_spk_kernel.coefs)
+            
+        if X_r is not None:
+            X_r_theta = np.einsum('tka,a->k', X_r, theta[ii:ii + len(self.r_kernel.coefs)])
+            a = a + X_r_theta
+#             print(np.mean(a[y==1]))
+            g_w_distance.append(np.mean(np.sum(X_r[:, y == 1, :], 0), 0) - np.mean(np.sum(X_r[:, y == 0, :], 0), 0))
+            ii += len(self.r_kernel.coefs)
+    
+        if X_r_spk is not None:
+            X_r_spk_theta = np.einsum('tka,a->k', X_r_spk, theta[ii:ii + len(self.r_spk_kernel.coefs)])
+            a = a + X_r_spk_theta
+#             print(np.mean(a[y==1]))
+            g_w_distance.append(np.mean(np.sum(X_r_spk[:, y == 1, :], 0), 0) - np.mean(np.sum(X_r_spk[:, y == 0, :], 0), 0))
+            ii += len(self.r_spk_kernel.coefs)
+            
+        if r_spk is not None:
+            a = a + theta[ii] * r_spk
+            g_w_distance.append(np.array([np.mean(r_spk[y == 1]) - np.mean(r_spk[y == 0])]))
+            ii += 1
+    
         w_distance = np.mean(a[y == 1]) - np.mean(a[y == 0])
-        g_w_distance = np.mean(np.sum(X_u[:, y == 1, :], 0), 0) - np.mean(np.sum(X_u[:, y == 0, :], 0), 0)
+        g_w_distance = np.concatenate((g_w_distance))
+        print(g_w_distance)
         h_w_distance = None
 
         return w_distance, g_w_distance, h_w_distance, None
 
-    def gh_objective(self,  dt, mask_spikes, X_u, y):
-        return self.gh_wasserstein_distance(dt, mask_spikes, X_u, y)
+    def gh_objective(self,  dt, mask_spikes, X_u, X_u_spk, r_spk, X_r, X_r_spk, y):
+        return self.gh_wasserstein_distance(dt, mask_spikes, X_u, X_u_spk, r_spk, X_r, X_r_spk, y)
             
-    def wasserstein_distance_kwargs(self, t, mask_spikes, u, y):
+    def wasserstein_distance_kwargs(self, t, mask_spikes, u, r, y):
 
         # args = np.where(shift_array(mask_spikes, 1, fill_value=False))
         # t_spk = (t[args[0]],) + args[1:]
         if self.u_kernel is not None:
             n_u_kernel = self.u_kernel.nbasis
             X_u = self.u_kernel.convolve_basis_continuous(t, u)
-            # X[:, :, 1:n_u_kernel + 1] = X_u
-        # X = X_u.copy()
-        # if self.u_kernel is not None:
-        #     n_eta = self.u_kernel.nbasis
-        #     X_eta = self.u_kernel.convolve_basis_discrete(t, t_spk, shape=mask_spikes.shape)
-        #     X = np.zeros(mask_spikes.shape + (1 + n_eta,))
-        #     X[:, :, 1:] = -X_eta
-        # else:
-        #     n_eta = 0
-        #     X = np.zeros(mask_spikes.shape + (1,))
+        else:
+            X_u = None
+            
+        if self.u_spk_kernel is not None:
+            n_u_spk_kernel = self.u_spk_kernel.nbasis
+            _u = u.copy()
+            _u[~mask_spikes] = 0
+            X_u_spk = self.u_spk_kernel.convolve_basis_continuous(t, _u)
+        else:
+            X_u_spk = None
+            
+        if self.r_kernel is not None:
+            n_r_kernel = self.r_kernel.nbasis
+            X_r = self.r_kernel.convolve_basis_continuous(t, r)
+        else:
+            X_r = None
+        
+        if self.r_spk_kernel is not None:
+            n_r_spk_kernel = self.r_spk_kernel.nbasis
+            _r = r.copy()
+            _r[~mask_spikes] = 0
+            X_r_spk = self.r_spk_kernel.convolve_basis_continuous(t, _r)
+        else:
+            X_r_spk = None
+        
+#         r_spk = np.array([np.sum(r[mask_spikes[:, sw], sw]) for sw in range(mask_spikes.shape[1])]) if 'r_spk' in self.features else None
+        r_spk = None if 'r_spk' not in self.features else np.array([np.sum(r[mask_spikes[:, sw], sw]) for sw in range(mask_spikes.shape[1])])
 
-        wasserstein_kwargs = dict(dt=get_dt(t), mask_spikes=mask_spikes, X_u=X_u, y=y)
+        wasserstein_kwargs = dict(dt=get_dt(t), mask_spikes=mask_spikes, X_u=X_u, X_u_spk=X_u_spk, r_spk=r_spk, X_r=X_r, 
+                                  X_r_spk=X_r_spk, y=y)
 
         return wasserstein_kwargs
 
-    def objective_kwargs(self, t, mask_spikes, u, y):
-        return self.wasserstein_distance_kwargs(t=t, mask_spikes=mask_spikes, u=u, y=y)
+    def objective_kwargs(self, t, mask_spikes, u, r, y):
+        return self.wasserstein_distance_kwargs(t=t, mask_spikes=mask_spikes, u=u, r=r, y=y)
--- gglm/glm/wglm.py
+++ gglm/glm/wglm.py
@@ -27,26 +27,57 @@ class WGLM(GLM):
 
         mask_spikes = np.concatenate((mask_spikes_te, mask_spikes_fr), axis=1)
         u = np.concatenate((u_te, u_fr), axis=1)
+        r = np.concatenate((r_te, r_fr), axis=1)
         y = np.concatenate((np.ones(n_samples_te), np.zeros(n_samples_fr)))
 
         # print(mask_spikes.shape, u.shape)
-        optimizer = self.critic.fit(t, mask_spikes, u, y, newton_kwargs=newton_kwargs_critic)
+        optimizer = self.critic.fit(t, mask_spikes, u, r, y, newton_kwargs=newton_kwargs_critic)
+        theta_critic = self.critic.get_params()
 
         log_likelihood = np.sum(u_te[mask_spikes_te]) - dt * np.sum(r_te)
         g_log_likelihood = np.sum(X_te[mask_spikes_te, :], axis=0) - dt * np.einsum('tka,tk->a', X_te, r_te)
         h_log_likelihood = - dt * np.einsum('tka,tk,tkb->ab', X_te, r_te, X_te)
 
         w_distance = optimizer.obj_iterations[-1]
-        c_w_distance = np.mean(self.critic.transform(t, mask_spikes_fr, u_fr))
-        g_c_w_distance = np.sum(self.critic.u_kernel.convolve_continuous(t, X_fr), axis=(0, 1)) / n_samples_fr
-        # print(c_w_distance, g_c_w_distance)
+        
+#         c_w_distance = np.mean(self.critic.transform(t, mask_spikes_fr, u_fr, r_fr))
+        c_w_distance = -w_distance
+        g_c_w_distance = np.zeros(len(theta))
         h_c_w_distance = np.zeros((len(theta), len(theta)))
+        
+        ii = 0
+        if self.critic.u_kernel is not None:
+            g_c_w_distance = g_c_w_distance + (np.sum(self.critic.u_kernel.convolve_continuous(t, X_fr), axis=(0, 1)) / n_samples_fr - np.sum(self.critic.u_kernel.convolve_continuous(t, X_te), axis=(0, 1)) / n_samples_te)
+#             g_c_w_distance = g_c_w_distance + np.sum(self.critic.u_kernel.convolve_continuous(t, X_fr), axis=(0, 1)) / n_samples_fr
+            ii += len(self.critic.u_kernel.coefs)
+    
+        if self.critic.r_kernel is not None:
+            g_fr = np.sum(self.critic.r_kernel.convolve_continuous(t, X_fr * np.exp(r_fr)[..., None]), axis=(0, 1)) / n_samples_fr
+            h_fr = np.sum(self.critic.r_kernel.convolve_continuous(t, np.einsum('tka,tkb->tkab', X_fr, X_fr) * np.exp(r_fr)[..., None, None]), axis=(0, 1)) / n_samples_fr
+            g_te = np.sum(self.critic.r_kernel.convolve_continuous(t, X_te * np.exp(r_te)[..., None]), axis=(0, 1)) / n_samples_te
+            h_te = np.sum(self.critic.r_kernel.convolve_continuous(t, np.einsum('tka,tkb->tkab', X_te, X_te) * np.exp(r_te)[..., None, None]), axis=(0, 1)) / n_samples_te
+            g_c_w_distance = g_c_w_distance + (g_fr - g_te)
+            h_c_w_distance = h_c_w_distance + (h_fr - h_te)
+            
+        if 'r_spk' in self.critic.features:
+            X_r_fr_spk = np.array([np.einsum('ta,t->a', X_fr[mask_spikes_fr[:, sw], sw, :], r_fr[mask_spikes_fr[:, sw], sw]) for sw in range(n_samples_fr)])
+            X_r_X_fr_spk = np.array([np.einsum('ta,t,tb->ab', X_fr[mask_spikes_fr[:, sw], sw, :], r_fr[mask_spikes_fr[:, sw], sw], X_fr[mask_spikes_fr[:, sw], sw, :]) for sw in range(n_samples_fr)])
+            X_r_te_spk = np.array([np.einsum('ta,t->a', X_te[mask_spikes_te[:, sw], sw, :], r_te[mask_spikes_te[:, sw], sw]) for sw in range(n_samples_te)])
+            X_r_X_te_spk = np.array([np.einsum('ta,t,tb->ab', X_te[mask_spikes_te[:, sw], sw, :], r_te[mask_spikes_te[:, sw], sw], X_te[mask_spikes_te[:, sw], sw, :]) for sw in range(n_samples_te)])
+            
+#             g_c_w_distance = g_c_w_distance + theta_critic[ii] * np.sum(X_r_fr_spk, 0) / n_samples_fr
+#             h_c_w_distance = h_c_w_distance + theta_critic[ii] * np.sum(X_r_X_fr_spk, 0) / n_samples_fr
+            g_c_w_distance = g_c_w_distance + theta_critic[ii] * (np.sum(X_r_fr_spk, 0) / n_samples_fr - np.sum(X_r_te_spk, 0) / n_samples_te)
+            h_c_w_distance = h_c_w_distance + theta_critic[ii] * (np.sum(X_r_X_fr_spk, 0) / n_samples_fr - np.sum(X_r_X_te_spk, 0) / n_samples_te)
+            ii += 1
+        # print(c_w_distance, g_c_w_distance)
+        
 
         obj = log_likelihood + c_w_distance
         g_obj = g_log_likelihood + g_c_w_distance
         h_obj = h_log_likelihood + h_c_w_distance
 
-        metrics = dict(w_distance=w_distance, c_w_distance=c_w_distance)
+        metrics = dict(w_distance=w_distance, c_w_distance=c_w_distance, w_distance2=optimizer.obj_iterations)
 
         return obj, g_obj, h_obj, metrics
 
