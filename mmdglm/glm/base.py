2f0a50123f7a6106f6ade63972306213536885f1
diegoarri91 <diegoarri91@gmail.com>
2020-11-20 18:26:31 -0500

    finished cleaning base glm

--- mmdglm/glm/base.py
+++ mmdglm/glm/base.py
@@ -13,7 +13,7 @@ class GLM:
         self.eta = eta
         self.noise = noise
 
-    def sample(self, t, stim=None, shape=(), full=False):
+    def sample(self, t, stim=None, shape=(), full_output=False):
 
         dt = get_dt(t)
         
@@ -30,8 +30,6 @@ class GLM:
             kappa_conv = np.pad(kappa_conv, ((1, 0),) + ((0, 0),) * (stim.ndim - 1))[:-1]
             kappa_conv = np.expand_dims(kappa_conv, axis=tuple(range(stim.ndim, len(shape))))
             u = u + kappa_conv
-#             padding = np.zeros((1,) + stim.shape[1:])
-#             kappa_conv = np.concatenate((padding, kappa_conv[:-1]), axis=0)
         else:
             kappa_conv = None
 
@@ -51,60 +49,66 @@ class GLM:
 
             j += 1
         
-        if full:
+        if full_output:
             return kappa_conv, eta_conv, u, r, mask_spikes
         else:
             return u, r, mask_spikes
 
-    def sample_conditioned(self, t, mask_spikes, stim=None, full=False):
+    def sample_conditioned(self, t, mask_spikes, stim=None, full_output=False):
 
         shape = mask_spikes.shape
         dt = get_dt(t)
-        arg_spikes = np.where(shift_array(mask_spikes, 1, fill_value=False))
-        t_spikes = (t[arg_spikes[0]],) + arg_spikes[1:]
         
-        if self.kappa is not None and stim is not None:
+        u = np.full(shape, self.u0)
+        arg_spikes = np.where(shift_array(mask_spikes, 1, fill_value=False))
+        t_spikes = (t[arg_spikes[0]], ) + arg_spikes[1:]
+        
+        if stim is not None:
             assert shape[:stim.ndim] == stim.shape
-            kappa_conv = np.concatenate((np.zeros((1,) + stim.shape[1:]), self.kappa.convolve_continuous(t, stim)[:-1]), axis=0)
-            u = kappa_conv + self.u0
-            for ii, dim in enumerate(shape[stim.ndim:]):
-                kappa_conv = np.stack([kappa_conv] * dim, ii + stim.ndim)
-                u = np.stack([u] * dim, ii + stim.ndim)
+            kappa_conv = self.kappa.convolve_continuous(t, stim)
+            kappa_conv = np.pad(kappa_conv, ((1, 0),) + ((0, 0),) * (stim.ndim - 1))[:-1]
+            kappa_conv = np.expand_dims(kappa_conv, axis=tuple(range(stim.ndim, len(shape))))
+            u = u + kappa_conv
         else:
-            kappa_conv = np.zeros(shape)
-            u = np.ones(shape) * self.u0
+            kappa_conv = None
             
         if self.eta is not None and len(t_spikes[0]) > 0:
-            eta_conv = self.eta.convolve_discrete(t, t_spikes, shape=shape[1:]) #TODO. check if 1: or not
+            eta_conv = self.eta.convolve_discrete(t, t_spikes, shape=shape[1:])
             u = u + eta_conv
         else:
             eta_conv = np.zeros(shape)
 
         r = np.exp(u)
 
-        if full:
+        if full_output:
             return kappa_conv, eta_conv, u, r
         else:
             return u, r
     
     def get_params(self):
+        
         n_kappa = 0 if self.kappa is None else self.kappa.nbasis
         n_eta = 0 if self.eta is None else self.eta.nbasis
         theta = np.zeros(1 + n_kappa + n_eta)
+        
         theta[0] = self.u0
         if self.kappa is not None:
             theta[1:1 + n_kappa] = self.kappa.coefs
         if self.eta is not None:
             theta[1 + n_kappa:] = self.eta.coefs
+            
         return theta
 
     def set_params(self, theta):
+        
         n_kappa = 0 if self.kappa is None else self.kappa.nbasis
+        
         self.u0 = theta[0]
         if self.kappa is not None:
             self.kappa.coefs = theta[1:1 + n_kappa]
         if self.eta is not None:
             self.eta.coefs = theta[1 + n_kappa:]
+            
         return self
 
     def likelihood_kwargs(self, t, mask_spikes, stim=None):
@@ -112,30 +116,24 @@ class GLM:
         n_kappa = 0 if self.kappa is None else self.kappa.nbasis
         n_eta = 0 if self.eta is None else self.eta.nbasis
 
-        X = np.zeros(mask_spikes.shape + (1 + n_kappa + n_eta,))
+        X = np.zeros(mask_spikes.shape + (1 + n_kappa + n_eta, ))
         X[..., 0] = 1
 
-        if self.kappa is not None and stim is not None:
-            n_kappa = self.kappa.nbasis
+        if stim is not None:
             X_kappa = self.kappa.convolve_basis_continuous(t, stim)
-            X_kappa = np.concatenate((np.zeros((1,) + X_kappa.shape[1:]), X_kappa[:-1]), axis=0)
-            for ii, dim in enumerate(mask_spikes.shape[stim.ndim:]):
-                X_kappa = np.stack([X_kappa] * dim, ii + stim.ndim)
+            X_kappa = np.pad(X_kappa, ((1, 0),) + ((0, 0),) * (X.ndim - 1))[:-1]
+            X_kappa = np.expand_dims(X_kappa, axis=tuple(range(X_kappa.ndim, X.ndim)))
             X[..., 1:1 + n_kappa] = X_kappa
         
         if self.eta is not None:
             args = np.where(shift_array(mask_spikes, 1, fill_value=False))
-            t_spk = (t[args[0]],) + args[1:]
-            n_eta = self.eta.nbasis
+            t_spk = (t[args[0]], ) + args[1:]
             X_eta = self.eta.convolve_basis_discrete(t, t_spk, shape=mask_spikes.shape)
             X[..., 1 + n_kappa:] = X_eta
 
         likelihood_kwargs = dict(dt=get_dt(t), X=X, mask_spikes=mask_spikes)
 
         return likelihood_kwargs
-
-    def objective_kwargs(self, t, mask_spikes, stim=None):
-        return self.likelihood_kwargs(t=t, mask_spikes=mask_spikes, stim=stim)
 
     def copy(self):
         return self.__class__(u0=self.u0, kappa=self.kappa.copy(), eta=self.eta.copy())
