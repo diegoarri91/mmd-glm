3d248ac3bb1b1cef5ca2853050b021f39c4121bd
diegoarri91 <diegoarri91@gmail.com>
2020-12-05 15:07:13 -0500

    corrected kernel

---
+++ mmdglm/convkernels/base.py
@@ -0,0 +1,122 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import fftconvolve
+
+from .utils import get_arg_support, get_dt, searchsorted
+
+
+class Kernel:
+
+    def __init__(self):
+        pass
+
+    def interpolate(self, t):
+        pass
+    
+    def interpolate_basis(self, t):
+        pass
+
+    def convolve_continuous(self, t, x):
+        """Implements the convolution of a time series with the kernel using scipy fftconvolve.
+
+        Args:
+            t (array): time points
+            x (array): time series to be convolved
+            mode (str): 
+
+        Returns:
+            array: convolved time series
+        """
+        
+        dt = get_dt(t)
+        arg_support0, arg_supportf = get_arg_support(dt, self.support)
+
+        t_support = np.arange(arg_support0, arg_supportf, 1) * dt
+        kernel_values = self.interpolate(t_support)
+        
+        shape = (kernel_values.shape[0], ) + tuple([1] * (x.ndim - 1))
+        kernel_values = kernel_values.reshape(shape)
+
+        convolution = np.zeros(x.shape)
+        
+        full_convolution = fftconvolve(kernel_values, x, mode='full', axes=0)
+
+        if arg_support0 >= 0:
+            convolution[arg_support0:, ...] = full_convolution[:len(t) - arg_support0, ...]
+        elif arg_support0 < 0 and arg_supportf >= 0: # or to arg_support0 < 0 and len(t) - arg_support0 <= len(t) + arg_supportf - arg_support0:
+            convolution = full_convolution[-arg_support0:len(t) - arg_support0, ...]
+        else: # or arg0 < 0 and len(t) - arg0 > len(t) + arg_supportf - arg0:
+            convolution[:len(t) + arg_supportf, ...] = full_convolution[-arg_supportf:, ...]
+                
+        convolution *= dt
+        
+        return convolution
+
+    def convolve_discrete(self, t, s, A=None, shape=None, renewal=False):
+        """Implements the convolution of discrete events in time with the kernel
+
+        Args:
+            t (array): time points
+            s (array): time events
+            mode (str): 
+
+        Returns:
+            array: convolved time series
+        """
+        
+        if type(s) is not tuple:
+            s = (s,)
+            
+        if A is None:
+            A = (1. for ii in range(s[0].size))
+
+        if shape is None:
+            shape = tuple([max(s[dim]) + 1 for dim in range(1, len(s))])
+
+        arg_s = searchsorted(t, s[0])
+        arg_s = np.atleast_1d(arg_s)
+
+        convolution = np.zeros((len(t), ) + shape)
+
+        for ii, (arg, A) in enumerate(zip(arg_s, A)):
+
+            index = tuple([slice(arg, None)] + [s[dim][ii] for dim in range(1, len(s))])
+            if not(renewal):
+                convolution[index] += A * self.interpolate(t[arg:] - t[arg])
+            else:
+                convolution[index] = A * self.interpolate(t[arg:] - t[arg])
+                
+        return convolution
+   
+    def fit(self, t, input, output, mask=None):
+
+        if mask is None:
+            mask = np.ones(input.shape, dtype=bool)
+
+        X = self.convolve_basis_continuous(t, input)
+        X = X[mask, :]
+        output = output[mask]
+
+        self.coefs = np.linalg.lstsq(X, output, rcond=None)[0]
+
+    def correlate_continuous(self, t, x):
+        return self.convolve_continuous(t, x[::-1])[::-1]
+        
+    def plot(self, t=None, ax=None, offset=0, invert_t=False, invert_values=False, exp_values=False, **kwargs):
+
+        if t is None:
+            t = np.linspace(self.support[0], self.support[1], 200)
+
+        if ax is None:
+            fig, ax = plt.subplots()
+
+        y = self.interpolate(t) + offset
+        if invert_t:
+            t = -t
+        if invert_values:
+            y = -y
+        if exp_values:
+            y = np.exp(y)
+        ax.plot(t, y, **kwargs)
+
+        return ax
---
+++ mmdglm/convkernels/values.py
@@ -0,0 +1,95 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import fftconvolve
+
+from .base import Kernel
+from .utils import get_dt, get_arg_support, searchsorted
+
+
+class KernelBasisValues(Kernel):
+
+    def __init__(self, dt, basis_values, support=None, coefs=None):
+        self.dt = dt
+        self.basis_values = basis_values
+        self.nbasis = basis_values.shape[1]
+        self.support = np.array(support) if support is not None else np.array([0, basis_values.shape[0] * dt])
+        self.coefs = np.array(coefs) if coefs is not None else np.ones(self.nbasis)
+        assert self.basis_values.shape[0] == int((self.support[1] - self.support[0]) / self.dt)
+
+    def interpolate(self, t):
+
+        assert np.isclose(self.dt, get_dt(t))
+
+        t = np.atleast_1d(t)
+        res = np.zeros(len(t))
+
+        arg0, argf = get_arg_support(self.dt, self.support, t0=t[0])
+
+        if arg0 >= 0:
+            argf = min(argf, len(t))
+            res[arg0:argf] = np.matmul(self.basis_values, self.coefs)[:argf - arg0]
+        elif arg0 < 0 and argf > 0:
+            n_times = self.basis_values.shape[0]
+            res[:min(len(t), n_times + arg0)] = np.matmul(self.basis_values, self.coefs)[-arg0:min(len(t) - arg0, n_times)]
+        
+        return res
+
+    def interpolate_basis(self, t):
+        
+        assert np.isclose(self.dt, get_dt(t))
+
+        t = np.atleast_1d(t)
+
+        arg0, argf = get_arg_support(self.dt, self.support, t0=t[0])
+
+        if arg0 >= 0:
+            argf = min(argf, len(t))
+            return self.basis_values[:argf - arg0]
+        elif arg0 < 0 and argf > 0:
+            n_times = self.basis_values.shape[0]
+            return self.basis_values[-arg0:min(len(t) - arg0, n_times)]
+
+    def convolve_basis_discrete(self, t, s, shape=None):
+
+        if type(s) is np.ndarray:
+            s = (s,)
+
+        arg_s = searchsorted(t, s[0])
+        arg_s = np.atleast_1d(arg_s)
+        arg0, argf = searchsorted(t, self.support)
+
+        if shape is None:
+            shape = tuple([len(t)] + [max(s[dim]) + 1 for dim in range(1, len(s))] + [self.nbasis])
+        else:
+            shape = shape + (self.nbasis, )
+
+        X = np.zeros(shape)
+        
+        for ii, arg in enumerate(arg_s):
+            indices = tuple([slice(arg, min(arg + argf, len(t)))] + [s[dim][ii] for dim in range(1, len(s))] + [slice(0, self.nbasis)])
+            X[indices] += self.basis_values[:min(arg + argf, len(t)) - arg, :]
+
+        return X
+
+    @classmethod
+    def orthogonalized_raised_cosines(cls, dt, last_time_peak, n, b, a=1e0, coefs=None):
+
+        range_locs = np.log(np.array([0, last_time_peak]) + b)
+        delta = (range_locs[1] - range_locs[0]) / (n - 1)  # delta = 1 / (n - 1) * np.log(1 + last_peak / b)
+        locs = np.linspace(range_locs[0], range_locs[1], n)
+
+        last_time = np.exp(range_locs[1] + 2 * delta / a) - b
+        t = np.arange(0, last_time, dt)
+        support = [t[0], t[-1] + dt]
+
+        raised_cosines = (1 + np.cos(np.maximum(-np.pi, np.minimum(
+            a * (np.log(t[:, None] + b) - locs[None, :]) * np.pi / delta / 2, np.pi)))) / 2
+        raised_cosines = raised_cosines / np.sqrt(np.sum(raised_cosines ** 2, 0))
+        u, s, v = np.linalg.svd(raised_cosines)
+        basis = u[:, :n]
+
+        return cls(basis_values=basis, support=support, dt=dt, coefs=coefs)
+
+    def copy(self):
+        kernel = KernelBasisValues(self.dt, self.basis_values.copy(), self.support.copy(), coefs=self.coefs.copy())
+        return kernel
