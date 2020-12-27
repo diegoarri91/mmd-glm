260d4a9347a6813f887a0d035c78611bd1add6f6
adehad <26027314+adehad@users.noreply.github.com>
2020-12-27 20:27:25 +0000

    Update setup.py requirements and packages

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
-from setuptools import setup
+from pathlib import Path
+from setuptools import find_namespace_packages, setup
 
-
-setup(name='mmd-glm',
-    version='0.1',
-    url='https://github.com/diegoarri91/mmd-glm',
-    author='Diego M. Arribas',
-    author_email='diegoarri91@gmail.com',
-    license='MIT',
-    install_requires=['numpy', 'scipy', 'torch'],
-    packages=['mmdglm']
-      )
+setup(
+    name="mmd-glm",
+    version="0.1",
+    url="https://github.com/diegoarri91/mmd-glm",
+    author="Diego M. Arribas",
+    author_email="diegoarri91@gmail.com",
+    license="MIT",
+    install_requires=[
+        "matplotlib",
+        "numpy",
+        "scipy",
+        "torch",
+    ],
+    packages=find_namespace_packages(str(Path(__file__).parent), include=["mmdglm*"]),
+)
